21f8630f3c146728fe0e59336dddce800f8a9e3d
WillThomasWeb <willthomas113@googlemail.com>
2020-04-03 23:48:26 +0100

    Update app.js
    Created Basic class for the timer functionality and added empty methods along with event listners.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("hello");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+    start() {
+        console.log("Play");
+    }
+    pause() {
+        console.log("pause");
+    }
+
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
