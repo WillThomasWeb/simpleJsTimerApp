90eda001de659d4f3bfa3b8504d9e3cab946dd85
WillThomasWeb <willthomas113@googlemail.com>
2020-04-04 01:01:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,22 +7,47 @@ class Timer {
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
-    start() {
-        console.log("Play");
-    }
-    pause() {
-        console.log("pause");
-    }
-    onDurationChange() {
 
-    }
-    tick() {
-    }
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+    pause = () => {
+        clearInterval(this.interval);
+    };
+    onDurationChange = () => {
 
+    };
+    tick = () => {
+        if (this.timeRemaining <=0){
+            this.pause();
+        }else {
+        this.timeRemaining = this.timeRemaining - 1;
+        }
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    };
+
+    set timeRemaining(time) {
+        return this.durationInput.value = time;
+    };
 }
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput,startButton,pauseButton);
+const timer = new Timer(durationInput,startButton,pauseButton, {
+    onStart() {
+
+    },
+    onTick() {
+
+    },
+    onComplete() {
+        
+    }
+});
+
